237a8f0974475725168f9812d0cc1a5a0a3e018b
DimpuChr <167059422+DimpuChr@users.noreply.github.com>
2024-08-25 21:59:29 +0530

    Initial commit for Spring Boot project

---
+++ spring-helm-demo/src/main/java/com/dar/devops/SpringHelmDemoApplication.java
@@ -0,0 +1,13 @@
+package com.dar.devops;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringHelmDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringHelmDemoApplication.class, args);
+	}
+
+}
---
+++ spring-helm-demo/src/test/java/com/dar/devops/SpringHelmDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dar.devops;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringHelmDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
